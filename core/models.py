bd3cd1d46f96134a6a0b9d4ee3ded9149d7db688
Saeid Askari <askari.saeed@gmail.com>
2019-03-06 13:48:11 -0400

    add project models and migrations

--- core/models.py
+++ core/models.py
@@ -5,3 +5,31 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email_confirmed = models.BooleanField(default=False)
+
+
+class Project(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    title = models.CharField(blank=False, max_length=300)
+    description = models.TextField()
+    deadline = models.DateField()
+    cost = models.IntegerField()
+    created = models.DateTimeField()
+
+
+class Milestone(models.Model):
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    deadline = models.DateField()
+    title = models.CharField(blank=False, max_length=300)
+    description = models.TextField()
+
+
+class Proposal(models.Model):
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    cost = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    employer_accepted = models.BooleanField(default=False)
+    employee_accepted = models.BooleanField(default=False)
+    created = models.DateTimeField()
+
+    def accepted(self):
+        return self.employee_accepted and self.employer_accepted
