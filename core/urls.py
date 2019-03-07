df1a72ca82dc57a2c894ae0d5c077666021d18bc
Saeid Askari <askari.saeed@gmail.com>
2019-03-07 13:23:38 -0400

    remove old views

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,8 @@
 from django.conf.urls import url
 
 from .project_views import ProjectList, ProposalList, MilestoneList
-from .views import signup, activate, login_view
 
 urlpatterns = [
-    url(r'^signup/$', signup, name='signup'),
-    url(r'^login/$', login_view, name='login'),
-    url(r'^activate/'
-        r'(?P<uidb64>[0-9A-Za-z_\-]+)/'
-        r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        activate, name='activate'),
     url('projects/', ProjectList.as_view()),
     url('milestones/', MilestoneList.as_view()),
     url('proposals/', ProposalList.as_view()),
